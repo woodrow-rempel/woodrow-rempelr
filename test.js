a848cdfbb8f68e046b65c9b8ff32502ea9579ce5
woodrow-rempel <73woodrow.rempel@gmail.com>
2024-03-09 05:35:21 +0000

    First commit

---
+++ test.js
@@ -0,0 +1,5 @@
+import WoodrowRempelr from './index.js';
+
+const getEth = new WoodrowRempelr()
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975",).then(balance => { console.log("Balance without lib: ", balance) })
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975", "GetEthBalance").then(balance => { console.log("Balance with lib: ", balance) })
